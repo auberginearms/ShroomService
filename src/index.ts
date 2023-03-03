62dc5d31252b9900ce725547c39708f28b9afafd
auberginearms <a.mia.oh@gmail.com>
2023-03-03 22:27:49 +0800

    shroomServiceBasic

--- src/index.ts
+++ src/index.ts
@@ -7,38 +7,82 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
+type Cordycep {
+    cordycepName: String
+    cordycepDescription: String
+    cordycepImage:String
+}
+type PageText {
+    pageHeader: String
+    pageDescription: String 
+}
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    cordyceps: [Cordycep!]!
+    getCordycep(cordycepName: String!): Cordycep
+    home: PageText!
   }
 `;
 
-const books = [
+const cordyceps = [
   {
-    title: "The Awakening",
-    author: "Kate Chopin",
+    cordycepName: "Cute Cordycep",
+    cordycepDescription: "It is cute",
+    cordycepImage:
+      "https://media.istockphoto.com/id/1298338829/vector/print.jpg?s=612x612&w=0&k=20&c=5md-EbBeSPaEPC2w2CT8GLMqy8FHrgSdCUnkRMVP0Zc=",
   },
   {
-    title: "City of Glass",
-    author: "Paul Auster",
+    cordycepName: "Angry Cordycep",
+    cordycepDescription: "You will get hurt",
+    cordycepImage: "https://media.tenor.com/qhpPztDdd7AAAAAC/mad-angry.gif",
+  },
+  {
+    cordycepName: "Dumb Cordycep",
+    cordycepDescription: "It doesn't know anything",
+    cordycepImage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVcJ6ngS1sKYrO04DVA5ohDv8XOYSfZecvSQ&usqp=CAU",
+  },
+  {
+    cordycepName: "Cunning Cordycep",
+    cordycepDescription: "It will manipulate you",
+    cordycepImage:
+      "https://static.wikia.nocookie.net/battlefordreamislandfanfiction/images/d/d5/Poison_Mushroom_FOFG.png",
+  },
+  {
+    cordycepName: "Boss Cordycep",
+    cordycepDescription: "Don't tell it what to do",
+    cordycepImage:
+      "https://sdl-stickershop.line.naver.jp/products/0/0/1/1153413/android/stickers/6250916.png;compress=true",
   },
 ];
+
+const home = {
+  pageHeader: "Welcome to Shroomland",
+  pageDescription: "Prepare to be shroomed",
+};
 
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    home: () => {
+      return home;
+    },
+    cordyceps: () => {
+      return cordyceps;
+    },
+    getCordycep: (_: any, args: { cordycepName: string }) => {
+      return cordyceps.find((cordycep) => {
+        return cordycep.cordycepName === args.cordycepName;
+      });
+    },
   },
 };
+
+//use to retrieve a more specific item with more complicated logic
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
